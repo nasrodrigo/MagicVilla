60399becee79d2f6022c17e2e64a4773db33a01d
Rodrigo Monteiro <rmonteiro@obsglobal.com>
2023-02-01 14:41:16 -0600

    Add project files.

---
+++ MagicVilla_API/Controllers/VillaApiController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+                new Villa { Id = 1, Name = "Pool view" },
+                new Villa { Id = 2, Name = "Beach view" }
+            };
+        }
+    }
+}
